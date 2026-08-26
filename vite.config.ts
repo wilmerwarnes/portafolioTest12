de3677a530776b66e9e46af0b407ba120accebf3
wilmerwarnes <wilmerwarnes@gmail.com>
2026-08-26 01:11:51 -0400

    Deploy to portafolioTest12: update base path

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   root: '.',
   publicDir: 'public',
+  base: '/portafolioTest12/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
